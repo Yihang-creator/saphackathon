9b003e3285c11eb40b3102924d39a01d0db27d98
yhe25 <yhe25@students.cs.ubc.ca>
2022-10-01 02:38:53 -0700

    fix errors

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/hello',
+    '/',
     createProxyMiddleware({
       target: 'http://localhost:8080',
       changeOrigin: true,
